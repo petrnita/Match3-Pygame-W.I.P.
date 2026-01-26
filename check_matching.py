9363219f82930f1c472ef1e29315c232583c951d
Petr Nita <petrmnita@gmail.com>
2026-01-26 21:41:18 +0100

    modified:   check_matching.py

--- check_matching.py
+++ check_matching.py
@@ -1,9 +1,11 @@
 from consts import GAME
 from game_assets import Gem
 
-mask = [
-    [[0, 1, 0],
+masks = [
+    [[0, 1, 0], # <- mask_row
      [1, 0, 1]],
+    # ^
+    # L mask_column
 
     [[1, 0, 1],
      [0, 1, 0]],
@@ -58,20 +60,32 @@ mask = [
      [1],
      [1]]
 ]
-
+'''
+    The class checks whether there is at least one possible match on the game board.
+        This check is performed using masks for 16 possible match combinations.
+            If the ones in the mask overlap with stones of the same color,
+        multiplying the mask by the board fields results in only two numbers:
+        the value representing the stone paired with the one, and zero.
+                    When the board fields do not match the mask,
+        more than one number will be generated at the positions of the ones.
+     Consequently, the set will contain values from the board fields and zero.
+    The check() function verifies if a set containing exactly two numbers occurs.
+                If so, the function terminates and returns True.
+If this case is not detected after iterating through the entire board, it returns False.
+'''
 
 class Check_Matching():
     @classmethod
     def check(cls, board: list[Gem]) -> bool:
-        for field_x in range(int(GAME.BOARD_SIZE.x)):
-            for field_y in range(int(GAME.BOARD_SIZE.y)):
-                for _mask in mask:
+        for board_x in range(int(GAME.BOARD_SIZE.x)):
+            for board_y in range(int(GAME.BOARD_SIZE.y)):
+                for mask in masks:
                     masked = set()
-                    for row in range(len(_mask)):
-                        for column in range(len(_mask[0])):
-                            masking_field = _mask[row][column]
-                            if row+field_y < int(GAME.BOARD_SIZE.y) and column+field_x < int(GAME.BOARD_SIZE.x):
-                                field = board[column+field_x][row+field_y]
+                    for mask_row in range(len(mask)):
+                        for mask_column in range(len(mask[0])):
+                            masking_field = mask[mask_row][mask_column]
+                            if mask_row+board_y < int(GAME.BOARD_SIZE.y) and mask_column+board_x < int(GAME.BOARD_SIZE.x):
+                                field = board[mask_column+board_x][mask_row+board_y]
                                 if field != None:
                                     masked.add(field.number*masking_field)
                                 else:
