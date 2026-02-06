9a65164c1ac79210b1e9e0fbe4f7cf3e8eb239b8
Petr Nita <petrmnita@gmail.com>
2026-02-06 07:06:56 +0100

    deleted:    Test.py

--- Test.py
+++
@@ -1,8 +0,0 @@
-from rich.console import Console
-
-console = Console()
-
-def log(message: str, headline: str='')
-    if headline != '':
-        console.rule(headline, align='left')
-    console.log(message)
